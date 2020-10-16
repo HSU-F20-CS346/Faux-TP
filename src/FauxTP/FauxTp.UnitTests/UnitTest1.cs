bd6f9e60c6f50f023b2c413fdc6fdef381c97b48
Ryan Beck <rb200@humboldt.edu>
2020-10-16 14:47:35 -0700

    Created the Base Unit Tests for our FTP which addressed Issue #6, and we finalized, for now, our design so we can start working which addressed Issue #4
    Signed-off-by: Ryan Beck <rb200@humboldt.edu>

--- src/FauxTP/FauxTp.UnitTests/UnitTest1.cs
+++ src/FauxTP/FauxTp.UnitTests/UnitTest1.cs
@@ -9,10 +9,126 @@ namespace FauxTp.UnitTests
         {
         }
 
-        [Test]
-        public void Test1()
+        /// <summary>
+        /// Tests to see if a file can be requested properly.
+        /// </summary>
+        [Test] 
+        public void FileRequest()
         {
-            Assert.Pass();
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see if the file is sent properly to to recipient
+        /// </summary>
+        [Test]
+        public void FileSend()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see if the file is received properly from the sender
+        /// </summary>
+        public void FileReceive()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests whether the authentication fails when a sedn or receive is sent through.
+        /// </summary>
+        public void AuthenticationFail()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see what happens if the client sends a request while the server is off.
+        /// </summary>
+        [Test]
+        public void ServerOff()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see what happens when the Server stops sending mid file
+        /// </summary>
+        [Test]
+        public void ServerPowerFailSend()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see what happens when the Server stops receiving mid file
+        /// </summary>
+        [Test]
+        public void ServerPowerFailReceive()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see what happens when the Client stops sending mid file
+        /// </summary>
+        [Test]
+        public void ClientPowerFailSend()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see what happens when the Client stops sending mid file
+        /// </summary>
+        [Test]
+        public void ClientPowerFailReceive()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see if there are non-standard chars in the filename
+        /// </summary>
+        public void NonStandardChars()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests what happens when a file is requested that doesn't exist
+        /// </summary>
+        [Test]
+        public void RequestingGhostFile()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Tests to see if overwriting a file works
+        /// </summary>
+        public void OverwritingFile()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Test for maximum filename length
+        /// </summary>
+        [Test]
+        public void FileNameTooLong()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Test to see if the file extension is a valid file extension.
+        /// </summary>
+        [Test]
+        public void ValidFileExtension()
+        {
+            Assert.Fail();
         }
     }
 }
