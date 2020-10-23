7c464115e600dc489e202ceeed95d84ced38e8e5
Vanja <vv52@humboldt.edu>
2020-10-23 00:45:26 -0700

    * Changed main UI to a three-panel design featuring a command section, a local file directory, and a remote file directory. File directories are currently empty windows but will contain lists of local and peer files
    * Dummy buttons have been added for download, upload, pause, resume, and cancel as well as matching methods with TODOs
    
    * Menu has been expanded. Now includes: a "Help" category that leads to a currently unimplemented help dialog; an "About" item under "File" that opens a dialog that displays basic information and a list of individuals that are contributing to the project; and a "Disconnect" item to the "Connect" menu with a dummy method

--- src/FauxTP/FauxTP.Terminal/Program.cs
+++ src/FauxTP/FauxTP.Terminal/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Net;
 using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography.X509Certificates;
 using Terminal.Gui;
@@ -13,10 +14,18 @@ namespace FauxTP.Terminal
             Application.Init();
             Colors.Base.Normal = Application.Driver.MakeAttribute(Color.Green, Color.Black);
 
+            // Main menu
             var menu = new MenuBar(new MenuBarItem[]
             {
                 new MenuBarItem ("_File", new MenuItem []
                 {
+                    // Show names of FauxTP team and other general information
+                    new MenuItem ("_About", "", () =>
+                    {
+                        OpenAboutDialog ();
+                    }),
+
+                    // Close application
                     new MenuItem ("_Quit", "", () =>
                     {
                         Application.RequestStop ();
@@ -24,33 +33,77 @@ namespace FauxTP.Terminal
                 }),
                 new MenuBarItem ("_Connect", new MenuItem []
                 {
+                    // Connect to server at specified IP with username and password
                     new MenuItem ("_Login", "", () =>
                     {
                         OpenLoginDialog ();
+                    }),
+
+                    // Disconnect from the current server
+                    new MenuItem ("_Disconnect", "", () =>
+                    {
+                        CloseConnection ();
+                    })
+                }),
+                new MenuBarItem ("_Help", new MenuItem []
+                {
+                    // Show basic functionality and link to documentation
+                    new MenuItem ("_Show Help", "", () =>
+                    {
+                        OpenHelpDialog ();
                     })
                 })
             });
 
-            var win = new Window("FauxTP")
+            // Command / control window
+            var fauxtp = new Window("FauxTP")
             {
                 X = 0,
                 Y = 1,
-                Width = Dim.Percent(50),
-                Height = Dim.Fill() - 1,
+                Width = Dim.Percent(34),
+                Height = Dim.Fill(),
                 ColorScheme = Colors.Base
             };
 
-            var dir = new Window("File Directory")
+            // Directory view of local files
+            var localDir = new Window("Local Directory")
             {
-                X = Pos.Percent(50),
+                X = Pos.Percent(34),
                 Y = 1,
-                Width = Dim.Percent(50),
-                Height = Dim.Fill() - 1,
+                Width = Dim.Percent(34),
+                Height = Dim.Fill(),
                 ColorScheme = Colors.Base
             };
 
-            // Add both menu and win in a single call
-            Application.Top.Add(menu, win, dir);
+            // Directory view of remote files
+            var peerDir = new Window("Peer Directory")
+            {
+                X = Pos.Percent(67),
+                Y = 1,
+                Width = Dim.Percent(34),
+                Height = Dim.Fill(),
+                ColorScheme = Colors.Base
+            };
+            
+            var download = new Button(5, 1, "     Download File     ") { };
+            download.Clicked += () => { DownloadFile(); };
+
+            var upload = new Button(5, 3, "      Upload File      ") { };
+            upload.Clicked += () => { UploadFile(); };
+
+            var pause = new Button(5, 5, "     Pause Transfer    ") { };
+            pause.Clicked += () => { PauseTransfer(); };
+
+            var resume = new Button(5, 7, "    Resume Transfer    ") { };
+            resume.Clicked += () => { ResumeTransfer(); };
+
+            var cancel = new Button(5, 9, "    Cancel Transfer    ") { };
+            cancel.Clicked += () => { CancelTransfer(); };
+
+            fauxtp.Add(download, upload, pause, resume, cancel);
+
+            // Add components to application window
+            Application.Top.Add(menu, fauxtp, localDir, peerDir);
             Application.Run();
         }
 
@@ -58,18 +111,10 @@ namespace FauxTP.Terminal
         {
             bool connectPressed = false;
             
-            var connect = new Button(16, 11, "Connect")
-            {
-                //Clicked = () => { Application.RequestStop(); connectPressed = true; }
-            };
-
+            var connect = new Button(16, 11, "Connect") {};
             connect.Clicked += () => { Application.RequestStop(); connectPressed = true; };
 
-            var cancel = new Button(31, 11, "Cancel")
-            {
-                //Clicked = () => Application.RequestStop()
-            };
-
+            var cancel = new Button(31, 11, "Cancel") {};
             cancel.Clicked += () => { Application.RequestStop(); };
             
             var dialog = new Dialog("Login", 60, 15, connect, cancel);
@@ -126,6 +171,66 @@ namespace FauxTP.Terminal
             {
                 //TODO: Add auth logic
             }
+        }
+
+        static void OpenAboutDialog()
+        {
+            var aboutInfo = new Label(
+                "Project for CS 346 @ HSU \n \n" +
+                "Designed by Vanja Venezia, Riley Heffernan, \n" +
+                "Fernando Crespo, James Pelligra, Candance M., \n" +
+                "Ryan Beck, Grayson Beckert, and Bradley Arline")
+            {
+                X = 1,
+                Y = 1
+            };
+
+            var ok = new Button(20, 7, "Cool!") { };
+            ok.Clicked += () => { Application.RequestStop(); };
+
+            var dialog = new Dialog("Login", 50, 11, ok);
+            dialog.Add(aboutInfo);
+
+            Application.Run(dialog);
+        }
+
+        static void OpenHelpDialog()
+        {
+            //TODO: Tutorialize basic features and link to docs in dialog box
+        }
+
+        static void CloseConnection()
+        {
+            //TODO: Close connection
+        }
+
+        static void DownloadFile()
+        {
+            //TODO: Download specified file from peer
+        }
+
+        static void UploadFile()
+        {
+            //TODO: Send specified file to peer
+        }
+
+        static void PauseTransfer()
+        {
+            //TODO: Pause transfer
+        }
+
+        static void ResumeTransfer()
+        {
+            //TODO: Resume transfer
+
+            // Potentially remove and implement both
+            // pause and resume functionality in the
+            // PauseTrasnfer() method using a bool
+        }
+
+        static void CancelTransfer()
+        {
+            //TODO: Cancel transfer
         }
     }
 }
