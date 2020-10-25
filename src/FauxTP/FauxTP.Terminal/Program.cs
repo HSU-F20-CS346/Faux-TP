1b18b194eb7102944d5647627182002d2e3a9f24
moorcan <moorcan85@gmail.com>
2020-10-24 20:48:12 -0700

    created Functions.cs with the following functions to see if a file exist or not for file replacement. Also created test in main -loadCSV -writeFile -fileExist -overwriteTest
    see issue #10
    
    Signed-off-by: Candace Moore <cdm103@humboldt.edu>

--- src/FauxTP/FauxTP.Terminal/Program.cs
+++ src/FauxTP/FauxTP.Terminal/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
+using FauxTP.Library;
 namespace FauxTP.Terminal
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string filename = "testfile2.csv";
+            bool shouldOverwrite = false;
+            
             Console.WriteLine("Hello World!");
+            string[] filecontents = Functions.loadCSV();
+            Functions.overwriteTest(filecontents, filename, shouldOverwrite);
         }
     }
 }
