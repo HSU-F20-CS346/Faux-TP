f080896dbb76d1a224f8c5d5ce6734cb396573cf
Ryan Beck <rb200@humboldt.edu>
2020-10-26 14:50:09 -0700

    Pushing the Authenticator certification though to a new branch
    Signed-off-by: Ryan Beck <rb200@humboldt.edu>

---
+++ src/FauxTP/FauxTP.Library/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FauxTP.Library
+{
+    class User
+    {
+        public string authKey { get; set; }
+
+        public string userAddress { get; set; }
+
+    }
+}
