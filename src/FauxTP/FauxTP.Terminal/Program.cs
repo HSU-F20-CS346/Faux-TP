44eb279210b893a94b8d7a575ebd1dcdce8a4a19
vv52 <54556895+vv52@users.noreply.github.com>
2020-10-26 16:34:26 -0700

    Revert "created Functions.cs with the following functions to see if a file ex…"

--- src/FauxTP/FauxTP.Terminal/Program.cs
+++ src/FauxTP/FauxTP.Terminal/Program.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using FauxTP.Library;
+
 namespace FauxTP.Terminal
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string filename = "testfile2.csv";
-            bool shouldOverwrite = false;
-            
             Console.WriteLine("Hello World!");
-            string[] filecontents = Functions.loadCSV();
-            Functions.overwriteTest(filecontents, filename, shouldOverwrite);
         }
     }
 }
